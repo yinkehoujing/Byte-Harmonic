32d57daee7a25a1d659ca6198f34d485f919cfd8
Yu <im.baifengyu@qq.com>
2025-04-27 20:06:37 +0800

    add: MainForm MouseMove BHColor FormStyle

---
+++ Forms/FormUtils/BHColor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Byte_Harmonic.Forms.FormUtils
+{
+    class MPColor
+    {
+        public static Color Grey1 = Color.FromArgb(250, 250, 250);
+        public static Color Grey2 = Color.FromArgb(238, 238, 238);
+        public static Color Grey3 = Color.FromArgb(224, 224, 224);
+        public static Color Grey4 = Color.FromArgb(189, 189, 189);
+        public static Color Grey5 = Color.FromArgb(117, 117, 117);
+    }
+}
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -1,13 +1,61 @@
+using Byte_Harmonic.Forms.FormUtils;
 using MySql.Data.MySqlClient;
 
-namespace ByteHarmonic.Forms;
-
-public class MainForm : Form
+namespace ByteHarmonic.Forms
 {
-
-    public MainForm()
+    /// <summary>
+    /// 主页探索页
+    /// </summary>
+    public class MainForm : Form
     {
+        private readonly MouseMove _mouseHandler;//用于鼠标控制窗口
+        private readonly FormStyle _styleHandler;//用于更改窗口样式
 
+        public MainForm()
+        {
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);//双缓冲减少闪烁
+            InitializeComponent();
+            _mouseHandler = new MouseMove(this);
+            _styleHandler = new FormStyle(this);
+        }
+
+        private void Home_Load(object sender, EventArgs e)//窗口加载
+        {
+            this.MaximizedBounds = Screen.PrimaryScreen.WorkingArea;//确定最大size
+        }
+
+        private void uiImageButton1_Click(object sender, EventArgs e)//关闭按钮
+        {
+            this.Close();
+        }
+
+        private void uiImageButton2_Click(object sender, EventArgs e)//最小化按钮
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+
+        private void uiImageButton5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // MainForm
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(1080, 720);
+            ControlBox = false;
+            FormBorderStyle = FormBorderStyle.None;
+            MaximumSize = new Size(1080, 720);
+            MinimumSize = new Size(1080, 720);
+            Name = "MainForm";
+            ResumeLayout(false);
+
+        }
     }
-
 }
+
