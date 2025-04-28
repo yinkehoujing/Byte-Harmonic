36345dffdabf4d40f65c57d5a6b3daf681018db5
yinkehoujing <153069815+yinkehoujing@users.noreply.github.com>
2025-04-28 09:19:55 +0800

    add: allowing console log

--- Program.cs
+++ Program.cs
@@ -9,10 +9,16 @@ namespace ByteHarmonic
         [STAThread]
         public static void Main()
         {
+            AllocConsole();
+            Console.WriteLine("start debugging……");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
             //Application.Run(new TestForm());
         }
+
+        // Windows API 调用：分配一个控制台窗口
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        static extern bool AllocConsole();
     }
 }
