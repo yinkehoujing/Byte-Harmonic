a85c863e2959dac30bbc2cf451c45e0cd6202520
Harmony Hong <1394603136@qq.com>
2025-05-07 17:27:08 +0800

    设置界面

--- Utils/ConfigManager.cs
+++ Utils/ConfigManager.cs
@@ -8,11 +8,34 @@ using System.Threading.Tasks;
 namespace Byte_Harmonic.Utils
 {
     // [[maybe unused]]
-    class ConfigManager
+    public class ConfigManager
     {
+        #region 连接数据库
         public static string GetConnectionString(string name)
         {
             return ConfigurationManager.ConnectionStrings[name].ConnectionString;
         }
+        #endregion
+
+        #region 下载
+        public string DownloadPath { get; set; }
+        public int NamingStyle { get; set; }
+
+        public void Save()
+        {
+            // 实现配置保存逻辑
+        }
+
+        public static ConfigManager Instance => _instance ??= LoadConfig();
+
+        private static ConfigManager _instance;
+
+        private static ConfigManager LoadConfig()
+        {
+            // 实现配置加载逻辑
+            return new ConfigManager();
+        }
+        #endregion
+
     }
 }
