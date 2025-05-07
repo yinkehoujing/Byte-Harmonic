db4367216ec9edd37c5643855020afc129c06e60
Harmony Hong <1394603136@qq.com>
2025-05-07 20:04:13 +0800

    修复小bug

--- Forms/MainForms/LoginForm.cs
+++ Forms/MainForms/LoginForm.cs
@@ -11,7 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Byte_Harmonic.Forms
+namespace Byte_Harmonic.Forms.MainForms
 {
     public partial class LoginForm : Form
     {
