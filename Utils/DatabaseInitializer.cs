3653597e7b527383e5124f91b352ad17d6e3b018
yinkehoujing <153069815+yinkehoujing@users.noreply.github.com>
2025-05-14 16:35:52 +0800

    更新数据初始化方式

--- Utils/DatabaseInitializer.cs
+++ Utils/DatabaseInitializer.cs
@@ -9,18 +9,33 @@ namespace Byte_Harmonic.Utils
 {
     public class DatabaseInitializer
     {
-        private readonly string _connectionString;
+        private readonly string _connectionString = "server=localhost;user=root;database=Byte_Harmonic;port=3306;password=";
+        private static string connectionString = "";
 
-        public DatabaseInitializer(string connectionString)
+        public DatabaseInitializer()
         {
-            _connectionString = connectionString;
+            if (!File.Exists(FileHelper.GetProjectRootPath("passwd.txt")))
+            {
+                Console.WriteLine("未找到本地根目录下 passwd.txt, 使用默认的连接字符串");
+                connectionString = _connectionString;
+            }
+            else
+            {
+                // 不要修改下面内容！！！
+                connectionString = "server=localhost;user=root;database=Byte_Harmonic;port=3306;password=";
+                var firstNonEmptyLine = File.ReadLines(FileHelper.GetProjectRootPath("passwd.txt"))
+                                            .Select(line => line.Trim())
+                                            .FirstOrDefault(line => !string.IsNullOrEmpty(line));
+
+                connectionString += firstNonEmptyLine;
+            }
         }
 
         public void ExecuteSqlFile(string filePath)
         {
             string sql = File.ReadAllText(FileHelper.GetSqlScriptPath(filePath));
 
-            using (var connection = new MySqlConnection(_connectionString))
+            using (var connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
 
